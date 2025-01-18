1cfd801979c42bd6b3c16d630e17362c04220704
Thomas Gandy <thomas@gandy.info>
2025-01-18 01:51:58 +0000

    Move the (unused) TCP state machine code to own file to reduce clutter.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"time"
+	"unsafe"
 )
 
 const (
@@ -22,38 +23,6 @@ const (
 	ControlBitMaskSyn                     = 1 << 1
 	ControlBitMaskFin                     = 1
 )
-
-const (
-	StateListen = iota
-	StateSynSent
-	StateSynReceived
-	StateEstablished
-	StateFinWait1
-	StateFinWait2
-	StateCloseWait
-	StateClosing
-	StateLastAck
-	StateTimeWait
-	StateClosed
-	TotalStates
-)
-
-const (
-	ActionActiveOpen = iota
-	ActionPassiveOpen
-	ActionClose
-	ActionReceiveSyn
-	ActionReceiveRst
-	ActionSend
-	ActionReceiveAckOfSyn
-	ActionReceiveSynAck
-	ActionReceiveFin
-	ActionReceiveAckOfFin
-	ActionTimeout
-	TotalActions
-)
-
-var stateTransitionTable = generateStateActionResultTransitionMap()
 
 type TransmissionControlBlock struct {
 	LocalIP, DestinationIP     uint32
@@ -141,54 +110,6 @@ func (machine *StateMachine) Transition(action int) error {
 	return nil
 }
 
-func generateStateActionResultTransitionMap() map[int]map[int]int {
-	result := make(map[int]map[int]int, TotalStates)
-
-	result[StateClosed] = make(map[int]int, TotalActions)
-	result[StateClosed][ActionActiveOpen] = StateSynSent
-	result[StateClosed][ActionPassiveOpen] = StateListen
-
-	result[StateListen] = make(map[int]int, TotalActions)
-	result[StateListen][ActionClose] = StateClosed
-	result[StateListen][ActionReceiveSyn] = StateSynReceived
-	result[StateListen][ActionSend] = StateSynSent
-
-	result[StateSynReceived] = make(map[int]int, TotalActions)
-	result[StateSynReceived][ActionReceiveRst] = StateListen
-	result[StateSynReceived][ActionReceiveAckOfSyn] = StateEstablished
-	result[StateSynReceived][ActionClose] = StateFinWait1
-
-	result[StateSynSent] = make(map[int]int, TotalActions)
-	result[StateSynSent][ActionClose] = StateClosed
-	result[StateSynSent][ActionReceiveSyn] = StateSynReceived
-	result[StateSynSent][ActionReceiveSynAck] = StateEstablished
-
-	result[StateEstablished] = make(map[int]int, TotalActions)
-	result[StateEstablished][ActionClose] = StateFinWait1
-	result[StateEstablished][ActionReceiveFin] = StateCloseWait
-
-	result[StateFinWait1] = make(map[int]int, TotalActions)
-	result[StateFinWait1][ActionReceiveFin] = StateClosing
-	result[StateFinWait1][ActionReceiveAckOfFin] = StateFinWait2
-
-	result[StateCloseWait] = make(map[int]int, TotalActions)
-	result[StateCloseWait][ActionClose] = StateLastAck
-
-	result[StateFinWait2] = make(map[int]int, TotalActions)
-	result[StateFinWait2][ActionReceiveFin] = StateTimeWait
-
-	result[StateClosing] = make(map[int]int, TotalActions)
-	result[StateClosing][ActionReceiveAckOfFin] = StateTimeWait
-
-	result[StateLastAck] = make(map[int]int, TotalActions)
-	result[StateLastAck][ActionReceiveAckOfFin] = StateClosed
-
-	result[StateTimeWait] = make(map[int]int, TotalActions)
-	result[StateTimeWait][ActionTimeout] = StateClosed
-
-	return result
-}
-
 type TcpEndpoint struct {
 	tcb        TransmissionControlBlock
 	receive    <-chan TcpSegment
@@ -202,7 +123,7 @@ func (endpoint *TcpEndpoint) Listen() {
 	for !shouldTerminate {
 		select {
 		case segment := <-endpoint.receive:
-			fmt.Printf("received segment %d\n", segment.Header.SeqNumber)
+			endpoint.receiveSegment(segment, uint16(unsafe.Sizeof(segment)))
 		case shouldTerminate = <-endpoint.terminate:
 			close(endpoint.send)
 			endpoint.terminated <- true
---
+++ statemachine.go
@@ -0,0 +1,81 @@
+package main
+
+const (
+	StateListen = iota
+	StateSynSent
+	StateSynReceived
+	StateEstablished
+	StateFinWait1
+	StateFinWait2
+	StateCloseWait
+	StateClosing
+	StateLastAck
+	StateTimeWait
+	StateClosed
+	TotalStates
+)
+
+const (
+	ActionActiveOpen = iota
+	ActionPassiveOpen
+	ActionClose
+	ActionReceiveSyn
+	ActionReceiveRst
+	ActionSend
+	ActionReceiveAckOfSyn
+	ActionReceiveSynAck
+	ActionReceiveFin
+	ActionReceiveAckOfFin
+	ActionTimeout
+	TotalActions
+)
+
+var stateTransitionTable = generateStateActionResultTransitionMap()
+
+func generateStateActionResultTransitionMap() map[int]map[int]int {
+	result := make(map[int]map[int]int, TotalStates)
+
+	result[StateClosed] = make(map[int]int, TotalActions)
+	result[StateClosed][ActionActiveOpen] = StateSynSent
+	result[StateClosed][ActionPassiveOpen] = StateListen
+
+	result[StateListen] = make(map[int]int, TotalActions)
+	result[StateListen][ActionClose] = StateClosed
+	result[StateListen][ActionReceiveSyn] = StateSynReceived
+	result[StateListen][ActionSend] = StateSynSent
+
+	result[StateSynReceived] = make(map[int]int, TotalActions)
+	result[StateSynReceived][ActionReceiveRst] = StateListen
+	result[StateSynReceived][ActionReceiveAckOfSyn] = StateEstablished
+	result[StateSynReceived][ActionClose] = StateFinWait1
+
+	result[StateSynSent] = make(map[int]int, TotalActions)
+	result[StateSynSent][ActionClose] = StateClosed
+	result[StateSynSent][ActionReceiveSyn] = StateSynReceived
+	result[StateSynSent][ActionReceiveSynAck] = StateEstablished
+
+	result[StateEstablished] = make(map[int]int, TotalActions)
+	result[StateEstablished][ActionClose] = StateFinWait1
+	result[StateEstablished][ActionReceiveFin] = StateCloseWait
+
+	result[StateFinWait1] = make(map[int]int, TotalActions)
+	result[StateFinWait1][ActionReceiveFin] = StateClosing
+	result[StateFinWait1][ActionReceiveAckOfFin] = StateFinWait2
+
+	result[StateCloseWait] = make(map[int]int, TotalActions)
+	result[StateCloseWait][ActionClose] = StateLastAck
+
+	result[StateFinWait2] = make(map[int]int, TotalActions)
+	result[StateFinWait2][ActionReceiveFin] = StateTimeWait
+
+	result[StateClosing] = make(map[int]int, TotalActions)
+	result[StateClosing][ActionReceiveAckOfFin] = StateTimeWait
+
+	result[StateLastAck] = make(map[int]int, TotalActions)
+	result[StateLastAck][ActionReceiveAckOfFin] = StateClosed
+
+	result[StateTimeWait] = make(map[int]int, TotalActions)
+	result[StateTimeWait][ActionTimeout] = StateClosed
+
+	return result
+}
